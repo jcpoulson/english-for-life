d4571db61792ebcfc401a2fa8d6dbfec0bf75087
jcpoulson <60674086+jcpoulson@users.noreply.github.com>
2020-12-01 19:43:08 -0500

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from psycopg2 import *
 
 
 
-DATABASE = PostgresqlDatabase('postgres://igotdvwfrjomru:6b9944648415a332b9e5cdf2442386bc9c18139103ebdc6557bac0886a570f2a@ec2-52-203-165-126.compute-1.amazonaws.com:5432/dcq2cjnuaud3h8')
+DATABASE = PostgresqlDatabase(host='postgres://igotdvwfrjomru:6b9944648415a332b9e5cdf2442386bc9c18139103ebdc6557bac0886a570f2a@ec2-52-203-165-126.compute-1.amazonaws.com:5432/dcq2cjnuaud3h8')
 
 class User(UserMixin, Model):
     id = AutoField()
